c710daf0be051a0e9aba424bb9bc7cac5d61dfcc
LOUISY_Ar <louisy.arnaud@gmail.com>
2023-01-23 12:23:05 +0100

    add: creation d'un classe database ainsi qu'un paktage + packtage appli ainsi que user + classe Password

---
+++ src/main/java/appli/User/Password.java
@@ -0,0 +1,15 @@
+package appli.User;
+package appli.user;
+
+import appli.StartApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import modele.User;
+import repository.EmployéRepository;
+
+import java.sql.SQLException;
+
+public class Password {
+}
