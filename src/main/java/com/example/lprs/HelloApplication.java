cc22ed5dd89560e46a10f704d7bd57df222f2ac4
francois <totofrancois12@gmail.com>
2023-01-23 09:35:49 +0100

    init : initialisation du projet java

---
+++ src/main/java/com/example/lprs/HelloApplication.java
@@ -0,0 +1,26 @@
+package com.example.lprs;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 450);
+        stage.setTitle("Lprs Intranet");
+        //Image icone = new Image("file:phoenix.jpg");
+        //stage.getIcons().add(icone);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
