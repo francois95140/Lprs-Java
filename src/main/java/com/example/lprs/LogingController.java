f3df09db38cab5139959f80f582ac50bdfad9b39
francois <totofrancois12@gmail.com>
2023-01-30 10:21:15 +0100

    Fix : loging finalisation

---
+++ src/main/java/com/example/lprs/LogingController.java
@@ -0,0 +1,38 @@
+package com.example.lprs;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+
+public class LogingController {
+
+    @FXML
+    private Label Errour;
+
+    @FXML
+    private Hyperlink PassewordForget;
+
+    @FXML
+    private MFXPasswordField passeword;
+
+    @FXML
+    private MFXButton seConnecter;
+
+    @FXML
+    private MFXTextField userName;
+
+    @FXML
+    void PassewordForget(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seConnecter(ActionEvent event) {
+
+    }
+
+}
