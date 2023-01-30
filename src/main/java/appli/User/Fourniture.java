2cb8f84d5d93d4a19259615e785367b1e32453dd
LOUISY_Ar <louisy.arnaud@gmail.com>
2023-01-30 10:01:25 +0100

    add: ajout class fournitu

---
+++ src/main/java/appli/User/Fourniture.java
@@ -0,0 +1,56 @@
+package appli.User;
+
+public class Fourniture {
+private int idFourniture;
+private String nom;
+private int stock;
+private String fournisseur;
+    private int code;
+
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+    public Fourniture(int idFourniture, String nom, int stock, String fournisseur){
+        this.idFourniture = idFourniture;
+        this.nom = nom;
+        this.stock = stock;
+        this.fournisseur = fournisseur;
+    }
+
+    public int getIdFourniture() {
+        return idFourniture;
+    }
+
+    public void setIdFourniture(int idFourniture) {
+        this.idFourniture = idFourniture;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getFournisseur() {
+        return fournisseur;
+    }
+
+    public void setFournisseur(String fournisseur) {
+        this.fournisseur = fournisseur;
+    }
+}
