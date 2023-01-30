abbd8867bd22770a858513b789c5740b8a4aba92
LOUISY_Ar <louisy.arnaud@gmail.com>
2023-01-30 10:01:47 +0100

    fix : fix bug this.cp

--- src/main/java/appli/User/FicheEtudiant.java
+++ src/main/java/appli/User/FicheEtudiant.java
@@ -16,7 +16,8 @@ private int telephone;
 private String rue;
 
 private String cp;
-private String Ville;
+private String ville;
+private int ref_utilisateur;
     private int code;
 
 
@@ -28,5 +29,97 @@ private String Ville;
         this.code = code;
     }
 
-    public FicheEtudiant(int idFiche, String nom, String prenom, String email,String diplome)
+    public FicheEtudiant(int idFiche, String nom, String prenom, String email, String diplome, int telephone, String rue, int cp,String ville, int ref_utilisateur){
+        this.idFiche = idFiche;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.diplome = diplome;
+        this.telephone = telephone;
+        this.rue = rue;
+        this.cp = String.valueOf(cp);
+        this.ville = ville;
+        this.ref_utilisateur = ref_utilisateur;
+
+    }
+
+    public int getIdFiche() {
+        return idFiche;
+    }
+
+    public void setIdFiche(int idFiche) {
+        this.idFiche = idFiche;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDiplome() {
+        return diplome;
+    }
+
+    public void setDiplome(String diplome) {
+        this.diplome = diplome;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getRue() {
+        return rue;
+    }
+
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
+
+    public String getCp() {
+        return cp;
+    }
+
+    public void setCp(String cp) {
+        this.cp = cp;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public int getRef_utilisateur() {
+        return ref_utilisateur;
+    }
+
+    public void setRef_utilisateur(int ref_utilisateur) {
+        this.ref_utilisateur = ref_utilisateur;
+    }
 }
