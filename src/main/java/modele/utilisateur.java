35dcfc548147af4fad993b75d5440d7a4c5c0832
francois <totofrancois12@gmail.com>
2023-02-06 09:24:55 +0100

    add : crea d'un nouveau controleur

--- src/main/java/modele/utilisateur.java
+++ src/main/java/modele/utilisateur.java
@@ -1,64 +1,76 @@
 package modele;
 
-public class utilisateur {
+public class Utilisateur {
+    private int idUtilisateur;
+    private String prenom;
+    private String nom;
+    private String email;
+    private String role;
+    private String mdp;
 
-
-        private int idUser;
-        private String prenom;
-        private String nom;
-        private String email;
-        private String role;
-        private String mdp;
-        private boolean estAdmin;
-
-    public utilisateur(int idUser, String prenom, String nom, String email, String role, String mdp, boolean estAdmin) {
-            this.idUser = idUser;
-            this.prenom = prenom;
-            this.nom = nom;
-            this.email = email;
-            this.role = role;
-            this.mdp = mdp;
-            this.estAdmin = estAdmin;
-
-        }
-
-        public String getPrenom() {
-            return prenom;
-        }
-
-        public void setPrenom(String prenom) {
-            this.prenom = prenom;
-        }
-
-        public String getNom() {
-            return nom;
-        }
-
-        public void setNom(String nom) {
-            this.nom = nom;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public String getRole() {
-            return role;
-        }
-
-        public void setRole(String role) {
-            this.role = role;
-        }
+    public Utilisateur(int idUtilisateur, String nom, String prenom, String email, String mdp, String role) {
+        this.idUtilisateur = idUtilisateur;
+        this.prenom = prenom;
+        this.nom = nom;
+        this.email = email;
+        this.role = role;
+        this.mdp = mdp;
     }
 
+    public Utilisateur(int idUtilisateur, String nom, String prenom, String email, String role) {
+        this.idUtilisateur = idUtilisateur;
+        this.prenom = prenom;
+        this.nom = nom;
+        this.email = email;
+        this.role = role;
+    }
 
+    public int getIdUtilisateur() {
+        return idUtilisateur;
+    }
 
+    public void setIdUtilisateur(int idUtilisateur) {
+        this.idUtilisateur = idUtilisateur;
+    }
 
+    public String getPrenom() {
+        return prenom;
+    }
 
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
 
+    public String getNom() {
+        return nom;
+    }
 
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+}
