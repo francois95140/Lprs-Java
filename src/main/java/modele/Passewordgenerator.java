32e821e432984310f76759b69cb5d44e68647fca
francois <totofrancois12@gmail.com>
2023-02-12 23:41:23 +0100

    add : generateur de mot de passe et de code de verification

--- src/main/java/modele/Passewordgenerator.java
+++ src/main/java/modele/Passewordgenerator.java
@@ -3,17 +3,18 @@ import java.util.Random;
 
 public class Passewordgenerator {
     private static final String ALPHABET_LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
-    private static final String ALPHABET_UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String ALPHABET_UPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String NUMBERS = "0123456789";
-    private static final String ALPHABET_AND_NUMBERS = ALPHABET_LOWERCASE + ALPHABET_UPPERCASE + NUMBERS;
-    private static Random random = new Random();
+    private static final String ALPHABET_AND_NUMBERS = ALPHABET_LOWERCASE + ALPHABET_UPERCASE + NUMBERS;
+    private static Random  random = new Random();
 
-    public static String generate(int length) {
+    public static String codeGenerate (int length){
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            int randomIndex = random.nextInt(ALPHABET_AND_NUMBERS.length());
-            sb.append(ALPHABET_AND_NUMBERS.charAt(randomIndex));
+        for (int i = 0 ; i < length ; i++){
+            int randomIdex = random.nextInt(ALPHABET_AND_NUMBERS.length());
+            sb.append(ALPHABET_AND_NUMBERS.charAt(randomIdex));
         }
         return sb.toString();
     }
+
 }
