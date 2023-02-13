fabe2b913822108c560a5c0bafc9294fd51e698f
francois <totofrancois12@gmail.com>
2023-02-13 01:09:34 +0100

    add : selection d'un utilisateur par son email

--- src/main/java/repository/UtilisateurRepository.java
+++ src/main/java/repository/UtilisateurRepository.java
@@ -18,7 +18,7 @@ public class UtilisateurRepository {
 
     public Utilisateur connexion(String email, String motDePasse) {
         Utilisateur Utilisateur = null;
-        String sql = "SELECT * FROM "+table+ " WHERE email=? and mdp=md5(?)";
+        String sql = "SELECT * FROM " + table + " WHERE email=? and mdp=md5(?)";
         PreparedStatement pstm;
         try {
             pstm = coBdd.getConnection().prepareStatement(sql);
@@ -26,8 +26,8 @@ public class UtilisateurRepository {
             pstm.setString(2, motDePasse);
             ResultSet rs = pstm.executeQuery();
             if (rs.next()) {
-                System.out.println(rs.getString("nom"));
-                Utilisateur = new Utilisateur(rs.getInt("id_user"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"),  rs.getString("mdp"),  rs.getString("role"));
+
+                Utilisateur = new Utilisateur(rs.getInt("id_user"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"), rs.getString("mdp"), rs.getString("role"));
             }
         } catch (SQLException e) {
 // TODO Auto-generated catch block
@@ -38,16 +38,36 @@ public class UtilisateurRepository {
 
     }
 
+    public Utilisateur getUserByMail(String email) {
+        Utilisateur utilisateur = null;
+        String sql = "SELECT * FROM " + table + " WHERE email=?";
+        PreparedStatement pstm;
+        try {
+            pstm = coBdd.getConnection().prepareStatement(sql);
+            pstm.setString(1, email);
+            ResultSet rs = pstm.executeQuery();
+            if (rs.next()) {
+                System.out.println(rs.getString("nom"));
+                utilisateur = new Utilisateur(rs.getInt("id_user"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"), rs.getString("mdp"), rs.getString("role"));
+            }
+        } catch (SQLException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return utilisateur;
+    }
+
     public ArrayList<Utilisateur> getUtilisateurs() {
         ArrayList<Utilisateur> Utilisateurs = new ArrayList<Utilisateur>();
         Utilisateur utilisateur;
-        String sql = "SELECT * FROM "+table;
+        String sql = "SELECT * FROM " + table;
         PreparedStatement pstm;
         try {
             pstm = coBdd.getConnection().prepareStatement(sql);
             ResultSet rs = pstm.executeQuery();
             while (rs.next()) {
-                utilisateur = new Utilisateur(rs.getInt("id_user"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"),  rs.getString("role"));
+                utilisateur = new Utilisateur(rs.getInt("id_user"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"), rs.getString("role"));
                 Utilisateurs.add(utilisateur);
             }
         } catch (SQLException e) {
@@ -57,10 +77,10 @@ public class UtilisateurRepository {
         return Utilisateurs;
     }
 
-    public void changepasseword (Utilisateur utilisateur) throws SQLException {
+    public void changepasseword(Utilisateur utilisateur) throws SQLException {
         String sql;
         PreparedStatement pstm;
-        sql = "UPDATE `"+table+"` SET `mdp`=md5(?) WHERE id_user=?";
+        sql = "UPDATE `" + table + "` SET `mdp`=md5(?) WHERE id_user=?";
         pstm = coBdd.getConnection().prepareStatement(sql);
         pstm.setString(1, utilisateur.getMdp());
         pstm.setInt(2, utilisateur.getIdUtilisateur());
