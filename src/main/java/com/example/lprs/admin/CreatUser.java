7ee1cd45c43110299d392faf54b50bb16fb59188
francois <totofrancois12@gmail.com>
2023-02-21 14:04:42 +0100

    Add: interface d'ajout d'utilisateur

--- src/main/java/com/example/lprs/admin/CreatUser.java
+++ src/main/java/com/example/lprs/admin/CreatUser.java
@@ -1,4 +1,59 @@
 package com.example.lprs.admin;
 
-public class CreatUser {
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import modele.Passewordgenerator;
+import modele.Utilisateur;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreatUser implements Initializable {
+
+    private int utilisateur;
+
+    public CreatUser (int u) {
+        this.utilisateur = u;
+        System.out.println(this.utilisateur);
+    }
+
+
+    @FXML
+    private Label Erreur;
+
+    @FXML
+    private MFXTextField email;
+
+    @FXML
+    private MFXTextField nom;
+
+    @FXML
+    private MFXTextField prenom;
+
+    @FXML
+    private MFXComboBox<String> role;
+
+    @FXML
+    private MFXButton valiez;
+
+    @FXML
+    void validez(ActionEvent event) {
+        System.out.println(Passewordgenerator.codeGenerate(8));
+        System.out.println(role.getItems().indexOf(role.getText())+1);
+    }
+
+    private final String[] roles = {
+            "Administrateur","Professeur","Secretaire","Gestionair de stock"
+    };
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        role.getItems().addAll(roles);
+    }
 }
+
