225e0793229a35d297b190642acbd17bf854c3c5
francois <totofrancois12@gmail.com>
2023-02-21 14:02:53 +0100

    Add: interface administrateur et controleur

--- src/main/java/com/example/lprs/admin/Accueil.java
+++ src/main/java/com/example/lprs/admin/Accueil.java
@@ -1,101 +1,97 @@
 package com.example.lprs.admin;
 
+import com.example.lprs.RunApplication;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXTableView;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import modele.Passewordgenerator;
 import modele.Utilisateur;
 import repository.UtilisateurRepository;
 
 import java.net.URL;
 import java.sql.SQLException;
-import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class Accueil implements Initializable {
 
-    @FXML
-    private TableView<Utilisateur> tbl;
-
-    @FXML
-    private Button btnAdd;
-
-    @FXML
-    private Button btnDelete;
-
-    @FXML
-    private Button btnModif;
-
     private Utilisateur utilisateur;
 
-    public Accueil(Utilisateur u) {
+    public Accueil (Utilisateur u) {
         this.utilisateur = u;
         System.out.println(this.utilisateur);
     }
 
+    @FXML
+    private MenuItem deletUser;
+
+    @FXML
+    private Tab demande;
+
+    @FXML
+    private MFXTableView<?> eleveListe;
+
+    @FXML
+    private MFXTableView<?> eleveListe1;
+
+    @FXML
+    private MFXTableView<?> listStock;
+
+    @FXML
+    private MFXTableView<?> listeDemande;
+
+    @FXML
+    private Tab listeEleve;
+
+    @FXML
+    private MenuItem newUser;
+
+    @FXML
+    private Tab personnel;
+
+    @FXML
+    private Tab profil;
+
+    @FXML
+    private Tab stokck;
+
+    @FXML
+    void onClickDeletUser(ActionEvent event) {
+        Passewordgenerator passe =new Passewordgenerator();
+        System.out.println("flag");
+    }
+
+    @FXML
+    void onClickNewUser(ActionEvent event) {
+        if (utilisateur.getRole().equals("1")){
+            RunApplication.changeScene("/com/example/lprs/admin/creat-user",new CreatUser(utilisateur.getIdUtilisateur()));
+            System.out.println(utilisateur.getIdUtilisateur());
+        }
+
+    }
+
+    @FXML
+    void onLogoutButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRowClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    private MFXComboBox<String> select;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        String[][] colonnes = {
-                {"Id user","idUtilisateur"},
-                {"Nom","nom"},
-                {"Prenom","prenom"},
-                {"Mail","email"},
-                {"Role","role"}
-        };
-        for (int i = 0 ; i < colonnes.length ; i ++){
-            TableColumn<Utilisateur,String> myTable= new TableColumn<>(colonnes[i][0]);
-            myTable.setCellValueFactory(new PropertyValueFactory<Utilisateur,String>(colonnes[i][1]));
-            tbl.getColumns().add(myTable);
-        }
-        UtilisateurRepository userRepository = new UtilisateurRepository();
-
-        tbl.getItems().addAll(userRepository.getUtilisateurs());
-
-        System.out.println(this.utilisateur.getRole());
-
-        if (!this.utilisateur.getRole().equals("1")){
-            btnAdd.setDisable(true);
-        }
-    }
-
-    @FXML
-    void AddAction(ActionEvent event) {
 
     }
 
-    @FXML
-    void delAction(ActionEvent event) throws SQLException {
-
-    }
-
-    @FXML
-    void editAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onSelIntem(MouseEvent event) {
-    }
-
-    @FXML
-    void menuAction(ActionEvent event) {
-        System.out.println("hey !");
-    }
-
-    @FXML
-    void deconnecterAction(ActionEvent event) {
-    }
-    @FXML
-    void editPasswordAction(ActionEvent event) {
-    }
-
-    @FXML
-    void editUserAction(ActionEvent event) {
-
-    }
 }
