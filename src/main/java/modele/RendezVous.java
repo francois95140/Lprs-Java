6f7f2cfe12ff97f372fd0ce334d4df111b5d5d79
abderahmane ben mechichi <abm93440@gmail.com>
2023-02-28 15:53:03 +0100

    RendezVous

--- src/main/java/modele/RendezVous.java
+++ src/main/java/modele/RendezVous.java
@@ -2,21 +2,32 @@ package modele;
 
 public class RendezVous {
 
-        private int id_rdv;
+        private int id_RendezVous;
 
         private String date;
         private String heure;
         private String salle;
         private String RefUtilisateur;
+        private int RefEleve;
 
-        public RendezVous( int id_rdv, String date, String heure, String salle, String RefUtilisateur) {
-            this.id_rdv = id_rdv;
+
+
+
+    public RendezVous(int id_rdv, String date, String heure, String salle, String RefUtilisateur, int RefEleve) {
+            this.id_RendezVous = id_rdv;
             this.date = date;
             this.heure = heure;
             this.salle = salle;
             this.RefUtilisateur = RefUtilisateur;
+            this.RefEleve = RefEleve;
         }
+            public int getId_RendezVous() {
+                return id_RendezVous;
+    }
 
+            public void setId_RendezVous(int id_RendezVous) {
+                 this.id_RendezVous = id_RendezVous;
+    }
         public String getDate() {
             return date;
         }
@@ -48,6 +59,14 @@ public class RendezVous {
         public void setRefUtilisateur(String RefUtilisateur) {
             this.RefUtilisateur = RefUtilisateur;
         }
+         public int getRefEleve() {
+         return RefEleve;
+    }
+
+         public void setRefEleve(int refEleve) {
+           RefEleve = refEleve;
+    }
+
     }
 
 
