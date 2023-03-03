0f34db7d109860a05148d8ccd2c18255e0184d2e
francois <totofrancois12@gmail.com>
2023-03-04 00:15:24 +0100

    Add : Tableau de visualisation des fiche etudiant

--- src/main/java/com/example/lprs/user/AccueilU.java
+++ src/main/java/com/example/lprs/user/AccueilU.java
@@ -3,7 +3,11 @@ package com.example.lprs.user;
 import com.example.lprs.RunApplication;
 import com.example.lprs.admin.CreatUser;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTableColumn;
 import io.github.palexdev.materialfx.controls.MFXTableView;
+import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
+import io.github.palexdev.materialfx.filter.IntegerFilter;
+import io.github.palexdev.materialfx.filter.StringFilter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,22 +16,21 @@ import javafx.scene.control.Tab;
 import javafx.scene.input.MouseEvent;
 import modele.FicheEtudiant;
 import modele.Utilisateur;
+import repository.FicheEtudiantRepository;
 
 import java.net.URL;
+import java.util.Comparator;
 import java.util.ResourceBundle;
 
 public class AccueilU implements Initializable {
 
+    FicheEtudiantRepository ficherepo = new FicheEtudiantRepository();
 
     private Utilisateur utilisateur;
     private FicheEtudiant ficheEtudiant;
 
     public AccueilU(Utilisateur u) {
         this.utilisateur = u;
-    }
-    public AccueilU(Utilisateur u,FicheEtudiant fiche) {
-        this.utilisateur = u;
-        this.ficheEtudiant = fiche;
     }
 
     @FXML
@@ -40,19 +43,23 @@ public class AccueilU implements Initializable {
     private Tab demande;
 
     @FXML
-    private MFXTableView<?> eleveListe;
+    private Tab dossier;
 
     @FXML
-    private MFXTableView<?> eleveListe1;
+    private Tab ficheEleve;
 
     @FXML
-    private MFXTableView<?> listStock;
+    private MFXTableView<String> listStock;
 
     @FXML
-    private MFXTableView<?> listeDemande;
+    private MFXTableView<String> listDemande;
 
     @FXML
-    private Tab listeEleve;
+    private MFXTableView<String> listDossier;
+
+
+    @FXML
+    private MFXTableView<FicheEtudiant> listEleve;
 
     @FXML
     private MFXButton modifMyAccont;
@@ -89,8 +96,60 @@ public class AccueilU implements Initializable {
         RunApplication.changeScene("/com/example/lprs/user/FicheEtudiant",new FicheControler(utilisateur));
     }
 
+    private void setupFiche() {
+        MFXTableColumn<FicheEtudiant> nomColumn = new MFXTableColumn<>("Nom", true, Comparator.comparing(FicheEtudiant::getNom));
+        MFXTableColumn<FicheEtudiant> prenomColumn = new MFXTableColumn<>("Prénom", true, Comparator.comparing(FicheEtudiant::getPrenom));
+        MFXTableColumn<FicheEtudiant> emailColum = new MFXTableColumn<>("Email",true, Comparator.comparing(FicheEtudiant::getEmail));
+        MFXTableColumn<FicheEtudiant> telColum = new MFXTableColumn<>("Téléphone", true, Comparator.comparing(FicheEtudiant::getTelephone));
+        MFXTableColumn<FicheEtudiant> diplomeColum = new MFXTableColumn<>("Doplôme", true, Comparator.comparing(FicheEtudiant::getDiplome));
+        MFXTableColumn<FicheEtudiant> adresseColum = new MFXTableColumn<>("Adresse", true, Comparator.comparing(FicheEtudiant::getAdresse));
+
+
+        nomColumn.setRowCellFactory(list -> new MFXTableRowCell<>(FicheEtudiant::getNom));
+        prenomColumn.setRowCellFactory(list -> new MFXTableRowCell<>(FicheEtudiant::getPrenom));
+        emailColum.setRowCellFactory(list -> new MFXTableRowCell<>(FicheEtudiant::getEmail));
+        telColum.setRowCellFactory(list -> new MFXTableRowCell<>(FicheEtudiant::getTelephone));
+        diplomeColum.setRowCellFactory(list -> new MFXTableRowCell<>(FicheEtudiant::getDiplome));
+        adresseColum.setRowCellFactory(list -> new MFXTableRowCell<>(FicheEtudiant::getAdresse));
+
+        nomColumn.setStyle("-fx-pref-width: 100");
+        prenomColumn.setStyle("-fx-pref-width: 100");
+        emailColum.setStyle("-fx-pref-width: 150");
+        telColum.setStyle("-fx-pref-width: 100");
+        diplomeColum.setStyle("-fx-pref-width: 100");
+        adresseColum.setStyle("-fx-pref-width: 200");
+
+        listEleve.getTableColumns().add(nomColumn);
+        listEleve.getTableColumns().add(prenomColumn);
+        listEleve.getTableColumns().add(emailColum);
+        listEleve.getTableColumns().add(telColum);
+        listEleve.getTableColumns().add(diplomeColum);
+        listEleve.getTableColumns().add(adresseColum);
+
+        listEleve.getFilters().add(new StringFilter<>("Nom", FicheEtudiant::getNom));
+        listEleve.getFilters().add(new StringFilter<>("Prénom", FicheEtudiant::getPrenom));
+        listEleve.getFilters().add(new StringFilter<>("Email", FicheEtudiant::getEmail));
+        listEleve.getFilters().add(new IntegerFilter<>("Téléphone", FicheEtudiant::getTelephone));
+        listEleve.getFilters().add(new IntegerFilter<>("Diplôme", FicheEtudiant::getTelephone));
+        listEleve.getFilters().add(new StringFilter<>("Adresse", FicheEtudiant::getAdresse));
+
+        listEleve.getItems().addAll(ficherepo.getFiche());
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        if (utilisateur.getRole()==2){
+            demande.setDisable(true);
+            stokck.setDisable(true);
+            setupFiche();
+        } else if (utilisateur.getRole()==3) {
+            ficheEleve.setDisable(true);
+            stokck.setDisable(true);
+        } else if (utilisateur.getRole()==4) {
+           ficheEleve.setDisable(true);
+           dossier.setDisable(true);
+        }
 
     }
 }
