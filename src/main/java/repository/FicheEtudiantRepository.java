7f61b546dd37204cd8c09d241e9cd2cca4a61a3d
francois <totofrancois12@gmail.com>
2023-03-03 01:02:19 +0100

    Add : getfiche Methode pour recuperation des Fiche Etudiant

--- src/main/java/repository/FicheEtudiantRepository.java
+++ src/main/java/repository/FicheEtudiantRepository.java
@@ -1,14 +1,13 @@
 package repository;
 import BDD.Database;
 import modele.FicheEtudiant;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
-public class  FicheEtudiantRepository
-{
+public class FicheEtudiantRepository {
     private Database coBdd;
     private String table = "fiche_etudiant";
 
@@ -16,26 +15,63 @@ public class  FicheEtudiantRepository
     public FicheEtudiantRepository() {
         coBdd = new Database();
     }
-    public FicheEtudiant inscription(FicheEtudiant etudiant)  throws SQLException{
 
-      String sql;
-      PreparedStatement pstm;
+    public FicheEtudiant save(FicheEtudiant etudiant) throws SQLException {
+        String sql;
+        PreparedStatement pstm;
+        if (etudiant.getIdFiche() > 0) {
+            sql = "UPDATE `" + table + "` SET `nom`=?,`prenom`=?,`diplome`=?,`email`=?,`telephone`=?, `rue`=?, `cp`=?, `ville`=?,ref_utilisateur=? WHERE id_fiche=?";
+            pstm = coBdd.getConnection().prepareStatement(sql);
 
-        sql = "INSERT INTO` "+table+" `(`nom`, `prenom`,`diplome`, `telephone`, `rue`, `cp`, `ville`) VALUES (?,?,?,?,?,?,?)";
-        pstm = coBdd.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        pstm.setString(1, etudiant.getNom());
-        pstm.setString(2, etudiant.getPrenom());
-        pstm.setString(3, etudiant.getDiplome());
-        pstm.setInt(4, etudiant.getTelephone());
-        pstm.setString(5, etudiant.getRue());
-        pstm.setInt(6, etudiant.getCp());
-        pstm.setString(7, etudiant.getVille());
-        pstm.executeUpdate();
-        ResultSet rs = pstm.getGeneratedKeys();
-        if (rs.next()){
-            int last_inserted_id = rs.getInt(1);
-            etudiant.setIdFiche(last_inserted_id);
+            pstm.setString(1, etudiant.getNom());
+            pstm.setString(2, etudiant.getPrenom());
+            pstm.setString(3, etudiant.getDiplome());
+            pstm.setString(4, etudiant.getEmail());
+            pstm.setInt(5, etudiant.getTelephone());
+            pstm.setString(6, etudiant.getRue());
+            pstm.setInt(7, etudiant.getCp());
+            pstm.setString(8, etudiant.getVille());
+            pstm.setInt(9, etudiant.getRef_utilisateur());
+            pstm.setInt(10, etudiant.getIdFiche());
+            pstm.executeUpdate();
+        } else {
+            sql = "INSERT INTO `" + table + "`(`nom`, `prenom`,`diplome`, `email`, `telephone`, `rue`, `cp`, `ville`,ref_utilisateur) VALUES (?,?,?,?,?,?,?,?,?)";
+            pstm = coBdd.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            pstm.setString(1, etudiant.getNom());
+            pstm.setString(2, etudiant.getPrenom());
+            pstm.setString(3, etudiant.getDiplome());
+            pstm.setString(4, etudiant.getEmail());
+            pstm.setInt(5, etudiant.getTelephone());
+            pstm.setString(6, etudiant.getRue());
+            pstm.setInt(7, etudiant.getCp());
+            pstm.setString(8, etudiant.getVille());
+            pstm.setInt(9, etudiant.getRef_utilisateur());
+            pstm.executeUpdate();
+            ResultSet rs = pstm.getGeneratedKeys();
+            if (rs.next()) {
+                int last_inserted_id = rs.getInt(1);
+                etudiant.setIdFiche(last_inserted_id);
+            }
         }
         return etudiant;
+    }
+
+    public ArrayList<FicheEtudiant> getFiche() {
+        ArrayList<FicheEtudiant> FicheEtudiant = new ArrayList<FicheEtudiant>();
+        FicheEtudiant fiche;
+        String sql = "SELECT * FROM " + table;
+        PreparedStatement pstm;
+        try {
+            pstm = coBdd.getConnection().prepareStatement(sql);
+            ResultSet rs = pstm.executeQuery();
+            while (rs.next()) {
+                fiche = new FicheEtudiant(rs.getInt("id_fiche"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"), rs.getString("diplome"), rs.getInt("telephone"), rs.getString("rue"), rs.getInt("cp"), rs.getString("ville"),rs.getInt("ref_utilisateur"));
+                FicheEtudiant.add(fiche);
+            }
+        } catch (SQLException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return FicheEtudiant;
     }
 }
