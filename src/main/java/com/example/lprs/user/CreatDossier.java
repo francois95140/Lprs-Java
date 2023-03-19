b1ddb3513c73271f7a539f41e74917db4291da60
francois <totofrancois12@gmail.com>
2023-03-19 01:25:27 +0100

    Add : Ajout de DossierInscripition

---
+++ src/main/java/com/example/lprs/user/CreatDossier.java
@@ -0,0 +1,4 @@
+package com.example.lprs.user;
+
+public class CreatDossier {
+}
--- src/main/java/modele/DossierInscripition.java
+++ src/main/java/modele/DossierInscripition.java
@@ -2,21 +2,24 @@ package modele;
 
 public class DossierInscripition {
 
-        private int id_dossier;
-        private String date;
-        private String heure;
-        private String filiere;
-        private String motivation;
-        private String ref_fiche;
+    private int id_dossier;
+    private String date;
+    private String eleve;
+    private String heure;
+    private String filiere;
+    private String motivation;
+    private int ref_fiche;
 
-        public DossierInscripition(int id_dossier, String date, String heure, String filiere, String motivation, String ref_fiche) {
-            this.id_dossier = id_dossier;
-            this.date = date;
-            this.heure = heure;
-            this.filiere = filiere;
-            this.motivation = motivation;
-            this.ref_fiche = ref_fiche;
-        }
+
+    public DossierInscripition(int id_dossier, String date, String heure, String filiere, String motivation, int ref_fiche, String eleven, String elevep) {
+        this.id_dossier = id_dossier;
+        this.date = date;
+        this.heure = heure;
+        this.filiere = filiere;
+        this.motivation = motivation;
+        this.ref_fiche = ref_fiche;
+        this.eleve = eleven + " " + elevep;
+    }
 
     public int getId_dossier() {
         return id_dossier;
@@ -27,45 +30,50 @@ public class DossierInscripition {
     }
 
     public String getDate() {
-            return date;
-        }
-
-        public void setDate(String date) {
-            this.date = date;
-        }
-
-        public String getHeure() {
-            return heure;
-        }
-
-        public void setHeure(String heure) {
-            this.heure = heure;
-        }
-
-        public String getFiliere() {
-            return filiere;
-        }
-
-        public void setFiliere(String filiere) {
-            this.filiere = filiere;
-        }
-
-        public String getMotivation() {
-            return motivation;
-        }
-
-        public void setMotivation(String motivation) {
-            this.motivation = motivation;
-        }
-
-        public String getRef_fiche() {
-            return ref_fiche;
-        }
-
-        public void setRef_fiche(String ref_fiche) {
-            this.ref_fiche = ref_fiche;
-        }
+        return date;
     }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getHeure() {
+        return heure;
+    }
+
+    public void setHeure(String heure) {
+        this.heure = heure;
+    }
+
+    public String getFiliere() {
+        return filiere;
+    }
+
+    public void setFiliere(String filiere) {
+        this.filiere = filiere;
+    }
+
+    public String getMotivation() {
+        return motivation;
+    }
+
+    public void setMotivation(String motivation) {
+        this.motivation = motivation;
+    }
+
+    public int getRef_fiche() {
+        return ref_fiche;
+    }
+
+    public void setRef_fiche(int ref_fiche) {
+        this.ref_fiche = ref_fiche;
+    }
+
+    public String getEleve() {
+        return eleve;
+    }
+
+}
 
 
 
